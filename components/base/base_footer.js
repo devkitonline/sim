1ed25b4afa67f3c43df9756b63823c9876dc9064
14520979 <14520979@gm.uit.edu.vn>
2021-10-03 22:24:27 +0700

    change to typescript

--- components/base/base_footer.js
+++
@@ -1,9 +0,0 @@
-export default function Base_footer() {
-    return (
-        <footer className="footer footer-transparent d-print-none">
-            <div className="container">
-
-            </div>
-        </footer>
-    )
-}
--- pages/api/base/menu.js
+++
@@ -1,4 +0,0 @@
-export default function handler(req, res) {
-    const data = require('../../../constants/menu.json');
-    res.status(200).json(data);
-}
