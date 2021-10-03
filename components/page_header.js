6bd35a9520251d7c55c7782ba0f31c3742d42bce
14520979 <14520979@gm.uit.edu.vn>
2021-10-03 12:19:42 +0700

    init

--- components/page_header.js
+++ components/page_header.js
@@ -6,23 +6,22 @@ function Page_header() {
         <header className="navbar navbar-expand-md navbar-light d-print-none">
             <div className="container-xl">
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-menu">
-                    <span className="navbar-toggler-icon"></span>
+                    <span className="navbar-toggler-icon"/>
                 </button>
                 <h1 className="navbar-brand navbar-brand-autodark d-none-navbar-horizontal pe-0 pe-md-3">
                     <a href=".">
-                        <img src="./static/logo.svg" width="110" height="32" alt="Tabler" className="navbar-brand-image">
+                        <Image src="/logo.png" width="110" height="32" alt="Tabler" className="navbar-brand-image">
                     </a>
                 </h1>
                 <div className="navbar-nav flex-row order-md-last">
                     <div className="nav-item dropdown d-none d-md-flex me-3">
                         <a href="#" className="nav-link px-0" data-bs-toggle="dropdown" tabIndex="-1" aria-label="Show notifications">
-                            <!-- Download SVG icon from http://tabler-icons.io/i/bell -->
                             <svg xmlns="http://www.w3.org/2000/svg" className="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                 <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                                 <path d="M10 5a2 2 0 0 1 4 0a7 7 0 0 1 4 6v3a4 4 0 0 0 2 3h-16a4 4 0 0 0 2 -3v-3a7 7 0 0 1 4 -6"/>
                                 <path d="M9 17v1a3 3 0 0 0 6 0v-1"/>
                             </svg>
-                            <span className="badge bg-red"></span>
+                            <span className="badge bg-red"/>
                         </a>
                         <div className="dropdown-menu dropdown-menu-end dropdown-menu-card">
                             <div className="card">
@@ -34,7 +33,7 @@ function Page_header() {
                     </div>
                     <div className="nav-item dropdown">
                         <a href="#" className="nav-link d-flex lh-1 text-reset p-0" data-bs-toggle="dropdown" aria-label="Open user menu">
-                            <span className="avatar avatar-sm" style="background-image: url(./static/avatars/000m.jpg)"></span>
+                            <span className="avatar avatar-sm" style={{}}></span>
                             <div className="d-none d-xl-block ps-2">
                                 <div>Paweł Kuna</div>
                                 <div className="mt-1 small text-muted">UI Designer</div>
--- components/page_menu.js
+++ components/page_menu.js
@@ -8,7 +8,7 @@ export default function Page_menu() {
                 <ul className="navbar-nav">
                     <li className="nav-item">
                         <a className="nav-link" href="./index.html">
-                    <span className="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/home -->
+                    <span className="nav-link-icon d-md-none d-lg-inline-block">
                         <svg xmlns="http://www.w3.org/2000/svg" className="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><polyline points="5 12 3 12 12 3 21 12 19 12"/><path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7"/><path d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6"/></svg>
                     </span>
                             <span className="nav-link-title">
@@ -18,7 +18,7 @@ export default function Page_menu() {
                     </li>
                     <li className="nav-item dropdown">
                         <a className="nav-link dropdown-toggle" href="#navbar-base" data-bs-toggle="dropdown" role="button" aria-expanded="false">
-                    <span className="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/package -->
+                    <span className="nav-link-icon d-md-none d-lg-inline-block">
                         <svg xmlns="http://www.w3.org/2000/svg" className="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><polyline points="12 3 20 7.5 20 16.5 12 21 4 16.5 4 7.5 12 3"/><line x1="12" y1="12" x2="20" y2="7.5"/><line x1="12" y1="12" x2="12" y2="21"/><line x1="12" y1="12" x2="4" y2="7.5"/><line x1="16" y1="5.25" x2="8" y2="9.75"/></svg>
                     </span>
                             <span className="nav-link-title">
